a5cfeafe658f675aff669fbf35ced4c22588500a
Simone Celia <simone.celia@simonecelia.it>
2024-04-30 08:03:29 +0200

    getall_impl

--- models/Text.php
+++ models/Text.php
@@ -19,12 +19,33 @@ class Text
 		return $stmt->execute();
 	}
 
-	public function getLast()
+	public function getLast($all): array
 	{
-		$query = "SELECT * FROM text ORDER BY id desc LIMIT 1;";
+		if ($all == 'yes') { // if query string 'all' == 'yes', get all values
+			$query = "SELECT * FROM text ORDER BY id desc";
+		} else {
+			$query = "SELECT * FROM text ORDER BY id desc LIMIT 1;";
+		}
 		$stmt = $this->conn->prepare($query);
 		$stmt->execute();
-
-		return $stmt->fetch(PDO::FETCH_ASSOC);
+		if ($all) {
+			$num = $stmt->rowCount();
+			if ($num > 0) {
+				$text_arr = array();
+				while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+					extract($row);
+					$text_item = array(
+						"id" => intval($id),
+						"text" => $text,
+						"created" => $created,
+					);
+					$text_arr[] = $text_item;
+				}
+				return $text_arr;
+			}
+		} else {
+			return $stmt->fetch(PDO::FETCH_ASSOC);
+		}
+		return [];
 	}
 }
